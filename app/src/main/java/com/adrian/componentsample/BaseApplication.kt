7f74f4287d785cbb3fbcdb932e7883823f6807c4
lyx0206331 <rq880113@163.com>
2019-05-10 18:32:37 +0800

    Initial commit

---
+++ app/src/main/java/com/adrian/componentsample/BaseApplication.kt
@@ -0,0 +1,15 @@
+package com.adrian.componentsample
+
+import android.app.Application
+
+/**
+ * date:2019/5/9 17:08
+ * author:RanQing
+ * description:
+ */
+class BaseApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ modulemain/src/main/java/com/adrian/modulemain/application/MainApplication.kt
@@ -0,0 +1,15 @@
+package com.adrian.modulemain.application
+
+import android.app.Application
+
+/**
+ * date:2019/5/9 17:09
+ * author:RanQing
+ * description:
+ */
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
